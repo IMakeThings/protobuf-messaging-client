a65997289b3a67644e91ea783dd680ffa9efe7c1
Patrick Laptop <patrick.christie4150@gmail.com>
2019-11-26 18:58:19 +1030

    my bals

--- src/main/java/com/github/protobuf_messaging_client/ProtobufMessagingClient.java
+++ src/main/java/com/github/protobuf_messaging_client/ProtobufMessagingClient.java
@@ -3,6 +3,8 @@ package com.github.protobuf_messaging_client;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import com.github.support.Logger;
@@ -13,60 +15,127 @@ import com.proto.messageServiceGrpc.messageServiceStub;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
 
 public class ProtobufMessagingClient {
 	private final ManagedChannel channel;
 	private final messageServiceBlockingStub blockingStub;
 	private final messageServiceStub asyncStub;
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
+	String userName;
+	
+	Random rand;
 	
 	public ProtobufMessagingClient(String host, int port) {
 		channel = ManagedChannelBuilder.forAddress(host, port).build();
 		blockingStub = messageServiceGrpc.newBlockingStub(channel);
 		asyncStub = messageServiceGrpc.newStub(channel);
+		
+		rand = new Random();
 	}
 	
-	public void shutdown() throws InterruptedException {
+	public void init() {
+		Logger.log("Enter a username");
+		try {
+			userName = br.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if(userName == null) {
+			userName = Integer.toString(rand.nextInt());
+		}
+		
+		Logger.log("Username: " + userName);
+	}
+	
+	public void disconnect() {
+		blockingStub.disconnect(Message.getDefaultInstance());
+		try {
+			shutdown();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void shutdown() throws InterruptedException {
 	    channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
 	}
 	
-	public void receiveMessages() {
+	public void sendMessage(Message message) {
 		try {
-			Logger.log("Enter a username");
-			String userName = br.readLine();
-			
-			final Message message = Message.newBuilder()
-					.setMessage("")
-					.setSender(userName)
-					.build();
-			
-			blockingStub.receiveMessages(message);
-			
-			StreamObserver<Message> responseObserver = new StreamObserver<Message>() {
-				@Override
-				public void onNext(Message value) {
-					Logger.log(value.getSender() + ":\t" + value.getMessage());
-				}
+			blockingStub.sendMessage(message);
+		} catch (StatusRuntimeException e) {
+			Logger.log("Unable to send message: " + message.getMessage());
+			return;
+		}
+	}
+	
+	public void receiveMessages() {
+		StreamObserver<com.proto.Message> responseObserver =
+				new StreamObserver<com.proto.Message>() {
 
-				@Override
-				public void onError(Throwable t) {
-					Logger.log(t.getMessage());
-				}
+					@Override
+					public void onCompleted() {
+						// TODO Auto-generated method stub
+						
+					}
 
-				@Override
-				public void onCompleted() {
-					// TODO Auto-generated method stub
-					
-				}
-			};
+					@Override
+					public void onError(Throwable arg0) {
+						// TODO Auto-generated method stub
+						
+					}
+
+					@Override
+					public void onNext(Message arg0) {
+						// TODO Auto-generated method stub
+						Logger.log(arg0.getMessage());
+					}
 			
-			asyncStub.receiveMessages(message, responseObserver);
+		};
+		
+		Message connectMessage = Message.newBuilder()
+				.setMessage("")
+				.setSender(userName)
+				.build();
+		
+		Iterator<Message> messages;
+		
+		asyncStub.receiveMessages(connectMessage, responseObserver);
+	}
+	
+	public static void main(String[] args) {
+		try {
+			Logger.log("Enter a host name");
+			String hostName = br.readLine();
+			Logger.log("Enter a port");
+			int port = Integer.parseInt(br.readLine());
 			
+			ProtobufMessagingClient pmc = new ProtobufMessagingClient(hostName, port);
+			pmc.init();
+//			pmc.receiveMessages();
+			
+			while(true) {
+				System.out.println("> ");
+				String m = br.readLine();
+				
+				Message message = Message.newBuilder()
+						.setSender(pmc.userName)
+						.setMessage(m)
+						.build();
+				
+				pmc.sendMessage(message);
+			}
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 		
 	}
 }
